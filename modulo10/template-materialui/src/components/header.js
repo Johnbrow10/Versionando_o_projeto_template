c5815c936311bf2037012d7ad723f7003e0211aa
johnbrow10 <iroman250@gmail.com>
2023-05-28 23:36:04 -0300

    feat(header): alterar o icone do header

--- modulo10/template-materialui/src/components/header.js
+++ modulo10/template-materialui/src/components/header.js
@@ -5,23 +5,22 @@ import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
 import IconButton from '@mui/material/IconButton';
-//import MenuIcon from '@mui/icons-material/Menu';
+import MenuIcon from '@mui/icons-material/Menu';
  
 function Header() {
     return (
         <Box sx={{ flexGrow: 1 }}>
-        <AppBar position="static">
+        <AppBar position="static" variant="elevation" enableColorOnDark>
           <Toolbar>
             <IconButton
               size="large"
               edge="start"
-              color="inherit"
               aria-label="menu"
-              sx={{ mr: 2 }}
+              sx={{ mr: 2, color: '#fff' }}
             >
-              {/*  
+               
               <MenuIcon />
-              */}
+              
             </IconButton>
             <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
               Gerenciamento de Projetos
